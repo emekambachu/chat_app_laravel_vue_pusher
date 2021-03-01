44adbe3df02b6b1ef7273b5e8c853ec9a96202a6
Emeka Mbachu <victor.c.mbachu@gmail.com>
2021-03-01 13:04:21 +0100

    first commit

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Chats</div>
+
+                    <div class="panel-body">
+                        <chat-messages-component :messages="messages"></chat-messages-component>
+                    </div>
+                    <div class="panel-footer">
+                        <chat-form-component
+                            v-on:messagesent="addMessage"
+                            :user="{{ Auth::user() }}"
+                        ></chat-form-component>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
